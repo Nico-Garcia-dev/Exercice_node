e07971774349a0f71f9015c2be08e0a83a8dc4a7
NICO GARCIA <07nico.garcia@gmail.com>
2025-03-24 11:44:15 +0100

    initial commit.

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const nom = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const Language = process.env.MY_LANGUAGE;
+
+console.log(`My name is ${nom}, I live in ${city} and I speak ${Language}`);	
